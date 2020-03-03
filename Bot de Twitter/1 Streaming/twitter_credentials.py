8380ca459b39d08702039a6b3250fe43bb3adddf
MissaelMata <60160657+MissaelMata@users.noreply.github.com>
2020-03-03 17:02:47 -0600

    Codigos

---
+++ Bot de Twitter/1 Streaming/twitter_credentials.py
@@ -0,0 +1,7 @@
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+# Variables that contains the user credentials to access Twitter API 
+
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
